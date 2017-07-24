7893cd8fb3f08b004d647813db0e382650766638
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-07-24 17:04:50 +0800

    24 item video item map

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-   // const video = props.videos;
-    return (
-        <ul className="col-md-4 list-group">
-            {props.videos.length}
-        </ul>    
-    );
-}
+        // const video = props.videos;
+        const videoItems = props.videos.map((video) => {
+                    return <VideoListItem video = { video }
+                    /> ;});
 
-export  default VideoList;
+                    return ( < ul className = "col-md-4 list-group" > { videoItems }
+
+                        <
+                        /ul>    
+                    );
+                }
+
+                export default VideoList;
